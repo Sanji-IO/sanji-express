8619adaca9f93c85039bcaed6499b44f65314b1f
Zack YL Shih <zackyl.shih@moxa.com>
2014-06-11 16:04:27 +0800

    update

--- test/all.js
+++ test/all.js
@@ -1,10 +1,30 @@
-var sanji = require('../lib/sanji'),
+var bunyan = require('bunyan'),
+    log = bunyan.log = bunyan.createLogger({name: 'Sanji-REST', level: 'debug'});
+    sanji = require('../lib/sanji'),
     express = require('express'),
     app = express();
 
-var bunyan = require('bunyan'),
-    log = bunyan.log = bunyan.createLogger({name: 'Sanji-REST', level: 'trace'});
+/**
+ * CORS support.
+ */
+function cros(req, res, next) {
+
+  if (!req.get('Origin')) {
+    return next();
+  }
+
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT');
+  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
+
+  if ('OPTIONS' === req.method) {
+    return res.send(200);
+  }
+
+  return next();
+}
 
 var rest = sanji(app, {modelPath: '/home/zack/samba/sanji-web/test/sample_config'});
-app.use(rest);
+app.use(cros);
+app.use('/v1/api', rest);
 app.listen(7890);
