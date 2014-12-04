fbf8d1c30ade8c3f430495f5c327b233fdcd5e34
Zack YL Shih <zackyl.shih@moxa.com>
2014-12-04 17:54:49 +0800

    sanji express with new sdk

--- test/all.js
+++ test/all.js
@@ -1,32 +1,12 @@
 var bunyan = require('bunyan'),
-    log = bunyan.log = bunyan.createLogger({name: 'Sanji-REST', level: 'debug'});
-    sanji = require('../lib/sanji'),
-    express = require('express'),
-    app = express();
+    log = bunyan.log = bunyan.createLogger({name: 'Sanji-REST', level: 'trace'}),
+    SanjiExpress = require('../lib/core'),
+    express = require('express');
 
-/**
- * CORS support.
- */
-function cros(req, res, next) {
+var app = express();
+var rest = new SanjiExpress(app, {
+  bundlePath: './sample_config'
+});
 
-  if (!req.get('Origin')) {
-    return next();
-  }
-
-  res.set('Access-Control-Allow-Origin', '*');
-  res.set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT');
-  res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-
-  if ('OPTIONS' === req.method) {
-    return res.send(200);
-  }
-
-  return next();
-}
-
-var rest = sanji(app, {
-    modelPath: './sample_config'
-  });
-app.use(cros);
-app.use('/v1/api', rest);
+// app.use(cros);
 app.listen(7890);
